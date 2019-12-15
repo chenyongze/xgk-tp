3dafbd9a213ac5c26533988b57f48c77da62278e
chenyongze <sapphire.php@gmail.com>
2019-12-15 17:43:36 +0800

    rediect 管理后台

---
+++ app/admin/controller/System.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\BaseController;
+
+class System extends BaseController
+{
+    public function index()
+    { 
+        return view();
+    }
+
+    public function menu()
+    { 
+        return view();
+    }
+
+
+}
