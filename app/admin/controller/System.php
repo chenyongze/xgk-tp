93cc553668f532b821771144e083adefb6112d6b
chenyongze <sapphire.php@gmail.com>
2019-12-15 18:08:18 +0800

    优化layout

--- app/admin/controller/System.php
+++ app/admin/controller/System.php
@@ -7,14 +7,22 @@ use app\BaseController;
 class System extends BaseController
 {
     public function index()
-    { 
-        return view();
+    {
+        return view('',['x', 3]);
     }
 
     public function menu()
-    { 
+    {
         return view();
     }
 
+    public function select_table()
+    {
+        return view('select-table');
+    }
 
+    public function table()
+    {
+        return view();
+    }
 }
